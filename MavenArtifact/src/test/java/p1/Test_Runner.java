dafc834c5defc851a0488eb31beecd9e77680347
IBM <IBM@DEVOPS_08>
2018-07-25 15:03:36 +0530

    v5

--- MavenArtifact/src/test/java/p1/Test_Runner.java
+++ MavenArtifact/src/test/java/p1/Test_Runner.java
@@ -1,9 +1,12 @@
+package p1;
 
 public class Test_Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			int a=3;
 			System.out.println("Helloooooooo.. how are youuuu?");
+			System.out.println(""+a*a);
 	}
 
 }
